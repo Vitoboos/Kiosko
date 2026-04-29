849be94613b8418d2ba06f37538261e541494d06
Victor Antonio Guaipo Mendoza <vguaipo@rolda.com.ve>
2026-04-29 14:31:10 -0400

    Ligeros ajustes de estilo

--- logic.js
+++ logic.js
@@ -14,3 +14,13 @@ function showSection(sectionId) {
 
     document.getElementById(sectionId).style.display = 'block';
 }
+
+function handleAgent() {
+    const teamsUrl = "https://teams.microsoft.com/l/app/?source=app-details-dialog&sharedAppResource=TaosSharedApp&titleId=T_65027c6c-8b00-fdfd-3581-edcaabe09419";
+    
+    const newWindow = window.open(teamsUrl, '_blank');
+    
+    setTimeout(() => {
+        if (newWindow) newWindow.close();
+    }, 500); 
+}
